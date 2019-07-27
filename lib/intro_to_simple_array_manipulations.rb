23f41c084dfe890ac03830b544cda2a3fbb796a6
Baktiar Miah <baktiar.m@gmail.com>
2019-07-27 18:04:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,9 +7,12 @@ def using_unshift (array, string)
 end 
 
 def using_pop (array)
-  array.pop(2)
+  array.pop()
 end 
-
+def pop_with_args(array)
+  array.pop(2)
+end
+  
 def using_shift (array)
   array.shift()
 end 
@@ -21,7 +24,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 def using_insert(array, element)
-  array[4].insert(element)
+  array[3].insert(element)
 end 
 def using_uniq(array)
   array.uniq()
